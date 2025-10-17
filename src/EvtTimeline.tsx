82f7fc12370f0a25346ebf0a5bb02ef725e87ec3
krapgras <ignacedecock@hotmail.com>
2025-10-17 23:29:36 +0200

    Add Host Mapping

--- src/EvtTimeline.tsx
+++ src/EvtTimeline.tsx
@@ -10,57 +10,69 @@ import ConnectIcon from "@mui/icons-material/ScreenShare";
 import DisconnectIcon from "@mui/icons-material/StopScreenShare";
 import AuthSuccessIcon from "@mui/icons-material/GppGood";
 
-const EvtTimeline = ({ events }: {events: any}) => {
+const hosts = [
+  ["192.168.0.23", "SERVER2.CONTOSO.COM"],
+  ["192.168.0.1", "SERVER1.CONTOSO.COM"],
+];
+
+const EvtTimeline = ({ events }: { events: any }) => {
+  const hostMap = Object.fromEntries(hosts);
+
   const timelineEvents = events ? (
-    events.map((event: any) => (
-      <TimelineItem key={event.timestamp}>
-        <TimelineOppositeContent
-          sx={{ m: "auto 0" }}
-          align="right"
-          variant="body2"
-          color="text.secondary"
-        >
-          {event.nice_timestamp}
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot
-            variant="outlined"
-            color={
-              event.type === "connected"
-                ? "success"
-                : event.type === "disconnected"
-                ? "warning"
-                : "success"
-            }
+    events.map((event: any) => {
+      const sourceStr = hostMap[event.source_ip] || event.source_ip;
+      const destStr = hostMap[event.destination] || event.destination;
+
+      return (
+        <TimelineItem key={event.timestamp}>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            variant="body2"
+            color="text.secondary"
           >
-            {event.type === "connected" ? (
-              <ConnectIcon color="success" />
-            ) : event.type === "disconnected" ? (
-              <DisconnectIcon color="warning" />
-            ) : (
-              <AuthSuccessIcon color="success" />
-            )}
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ py: "12px", px: 2 }}>
-          <Typography variant="h6" component="span">
-            {event.type.charAt(0).toUpperCase() + event.type.slice(1)}
-          </Typography>
-          <Typography>
-            {event.source_ip}{" "}
-            <Typography variant="body2" sx={{ color: "text.info" }}>
-              &rarr;
-            </Typography>{" "}
-            {event.destination}
-          </Typography>
-          <Typography color="secondary">
-            {event.domain}\{event.user}
-          </Typography>
-        </TimelineContent>
-      </TimelineItem>
-    ))
+            {event.nice_timestamp}
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot
+              variant="outlined"
+              color={
+                event.type === "connected"
+                  ? "success"
+                  : event.type === "disconnected"
+                  ? "warning"
+                  : "success"
+              }
+            >
+              {event.type === "connected" ? (
+                <ConnectIcon color="success" />
+              ) : event.type === "disconnected" ? (
+                <DisconnectIcon color="warning" />
+              ) : (
+                <AuthSuccessIcon color="success" />
+              )}
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "12px", px: 2 }}>
+            <Typography variant="h6" component="span">
+              {event.type.charAt(0).toUpperCase() + event.type.slice(1)}
+            </Typography>
+            <Typography>
+              {sourceStr}{" "}
+              <Typography variant="body2" sx={{ color: "text.info" }}>
+                &rarr;
+              </Typography>{" "}
+              {destStr}
+            </Typography>
+            <Typography color="secondary">
+              {event.domain}\{event.user}
+            </Typography>
+          </TimelineContent>
+        </TimelineItem>
+      );
+    })
   ) : (
     <></>
   );
